67f812cb1ec3762eaa90300f95f7f73a73682d27
aqib9393 <shaikh.boss2@gmail.com>
2026-08-17 01:13:37 +0200

    ready to deply

--- app.py
+++ app.py
@@ -4,5 +4,6 @@ st.set_page_config(page_title="My App", page_icon="✅")
 
 st.title("My App")
 st.write("App is working fine")
+st.write("App ready to deploy for artificial intelligence and machine learning projects.")
 
 st.success("Status: ok")
